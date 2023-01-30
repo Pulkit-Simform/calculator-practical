65af5a8ea488a931e5eeda26c1f85eaa0a20a41c
Pulkit Sharma <pulkit@simformsolutions.com>
2023-01-30 12:40:16 +0530

    class and methods implemented for event listeners

--- script.js
+++ script.js
@@ -1,12 +1,78 @@
-let str = "0"
 
-let textDisplay = document.getElementById("displayStr")
+let str = "0";
+let calculateStr = [];
+
+class ListenerEventsForNumbers{
+
+    #elementMap = new Map([
+        ["zero","0"],
+        ["one","1"],
+        ["two","2"],
+        ["three","3"],
+        ["four","4"],
+        ["five","5"],
+        ["six","6"],
+        ["seven","7"],
+        ["eight","8"],
+        ["nine","9"]
+    ])
+
+    #textDisplay = document.getElementById("displayStr")
+    #elementFromListener = null;
+    
 
 
-document.getElementById("pi")?.addEventListener("click",() =>{
-    str = "3.14"  
+    constructor(elementId){
+        this.elementId = elementId;        
+        this.#textDisplay.textContent = str;
+        this.#elementFromListener = document.getElementById(this.elementId)
+    }
+
+
+    // listener callback function that retrun callback : higher order function
+    #listenerFunction(callbackfn){
+        this.#elementFromListener.addEventListener("click",callbackfn)
+    }
+
+
+    // Adding events for individual units
+    //events for number
+    _listeningNumberEvents = () => {
+        this.#listenerFunction(() => {
+            str = str === "0" ? this.#elementMap.get(this.elementId).toString() : str.concat(this.#elementMap.get(this.elementId).toString())            
+            this.#textDisplay.textContent = str
+        })       
+    }
+
+    //events for arithmetic operators
+    _listeningArithmeticEvents(){
+        this.#listenerFunction(() => {
+            calculateStr.push(str,this.#elementFromListener.textContent)
+            this.#textDisplay?.textContent = "0";
+        })
+    }
+
+}
+
+// Calculate and eventListening for numbers
+let numberArray = ["one","two","three","four","five","six","seven","eight","nine","zero"]
+
+numberArray.forEach(i => {
+    let obj = new ListenerEventsForNumbers(i)
+    obj._listeningNumberEvents()
 })
 
-textDisplay.textContent = str;
+
+// Event Listening for arithmetic operator
+
+const arithmeticOperatorArray = ["addition","subtraction","multiply","division"] 
+
+
+
+
+
+
+
+
 
 
